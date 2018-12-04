3c990118564797c161fcad334b92f16e5631365b
EvdochukArtem <evdochukartem@gmail.com>
2018-12-05 00:42:52 +0300

    Initial commit

---
+++ Dubstep/Dubstep/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dubstep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SongDecoder("RWUBWUBWUBLWUBWUB"));
+            Console.ReadLine();
+        }
+        public static string SongDecoder(string input)
+        {
+            /*input = input.Replace("WUB", "!");
+            for (int i = 0; i < input.Length; i++)
+            {
+                int a = input.IndexOf("!!");
+                input = input.Remove(input.IndexOf("!!"), 1);
+            }*/
+            return input.Replace("WUB", "!").Replace("!!", "!").Trim('!');
+        }
+    }
+}
---
+++ Rectangle into Squares/Rectangle into Squares/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_into_Squares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqInRect.sqInRect(20, 14);
+            SqInRect.sqInRect(5, 5);
+            Console.ReadLine();
+        }
+    }
+
+    public class SqInRect
+    {
+
+        public static List<int> sqInRect(int lng, int wdth)
+        {
+            List<int> sqrs = new List<int>() {};
+            if (lng != wdth)
+            {
+                int rsqt = lng * wdth;
+                int sqr = 0;
+                int a = Math.Min(lng, wdth);
+                while (a * a > rsqt) a--;
+                while (sqr < rsqt)
+                {
+                    if (sqr + a * a > rsqt) a--;
+                    else
+                    {
+                        sqr += a * a;
+                        sqrs.Add(a);
+                        if (sqr == rsqt)
+                        {
+                            foreach (int Sq in sqrs)
+                            {
+                                Console.Write(Sq);
+                            }
+                            Console.WriteLine();
+                            return sqrs;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
+}
