a1d42f22ae72e53add341ef3bdcccbc86bfbb752
EvdochukArtem <45609233+EvdochukArtem@users.noreply.github.com>
2018-12-11 16:03:42 +0300

    Roman Numerals Encoder

--- Roman Numerals Encoder/Roman Numerals Encoder/Program.cs
+++ Roman Numerals Encoder/Roman Numerals Encoder/Program.cs
@@ -10,7 +10,33 @@ namespace Roman_Numerals_Encoder
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine(RomanConvert.Solution(2008));
+            Console.ReadLine();
+        }
+    }
+    public class RomanConvert
+    {
+        public static string Solution(int n)
+        {
+            string result = "", digit, roman = "IVXLCDM";
+            int j = 0, a;
+            while (n > 0)
+            {
+                a = 0; digit = "";
+                while (n % 10 - a > 0)
+                {
+                    digit += roman[j * 2];
+                    a++;
+                }
+                if (a % 5 == 4)
+                    digit = digit.Remove(1) + roman[j * 2 + a / 4];
+                if (a / 5 > 0)
+                    digit = digit.Remove(a % 5)
+                                 .Insert(0, roman[j *2 + a / 5].ToString());
+                result = result.Insert(0, digit);
+                n /= 10; j++;
+            }
+            return result;
         }
     }
 }
